412b6da409138715d099f475cf3673295ec5f649
Laoye99 <opeyemioladunjoye73@gmail.com>
2023-11-21 12:39:02 +0100

    first commit

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const INITIAL_STATE = {
+  currentUser: null,
+  test: {a: 1},
+};
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState: INITIAL_STATE,
+  reducers: {
+
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+
+    },
+  },
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
+
